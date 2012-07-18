3fbb8fe70708f308eb81960edcb746890814f6c1
Andy Fundinger <andy@makerbar.com>
2012-07-17 21:45:37 -0400

    added config module

---
+++ makerfinance/config.py
@@ -0,0 +1,11 @@
+from ConfigParser import SafeConfigParser
+
+__author__ = 'andriod'
+
+config = None
+
+def init(cfg_file = 'makerfinance.ini'):
+    global config
+    parser = SafeConfigParser()
+    parser.read(cfg_file)
+    config = parser
